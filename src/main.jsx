2161cb03310d1eb58f53e70707dc0dfb9f0c3ebf
goodvibesreports-cell <goodvibes.reports@gmail.com>
2025-11-25 02:50:25 +0100

    frontend reinstall

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// Root Element finden
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
   throw new Error(
-    "Root-Element fehlt! Stelle sicher, dass <div id='root'></div> in index.html steht."
+    "Root-Element fehlt! Stelle sicher, dass <div id=\"root\"></div> in index.html steht."
   );
 }
 
-// Render
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
